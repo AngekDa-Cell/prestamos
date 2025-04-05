21e6f60ea09dcc1b1e0334395ae1b9ed908ebac4
AngekDa-Cell <angeldavidpadillachavez@gmail.com>
2025-04-04 23:22:50 -0600

    Prestamos Terminado

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,42 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href={{ secure_asset('bootstrap-5.3.3-dist/css/bootstrap.min.css')}} />
+    <script src={{ secure_asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}></script>
+    <link href={{ secure_asset('DataTables/datatables.css') }} rel="stylesheet"/>
+    <script src={{ secure_asset('DataTables/datatables.js') }}></script>
+    <link href={{ secure_asset('assets/style.css') }} rel="stylesheet" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Préstamos Colima</title>
-    
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/bootstrap-5.3.3-dist/css/bootstrap.min.css" />
-    
-    <!-- DataTables CSS -->
-    <link href="/DataTables/datatables.css" rel="stylesheet"/>
-    
-    <!-- Custom CSS -->
-    <link href="/assets/style.css" rel="stylesheet" />
 </head>
-<body>
-    <div class="container-fluid">
+
+    <body>
         <div class="row">
             <div class="col-2">
-                @component("components.sidebar")
-                @endcomponent
-            </div>
-            <div class="col-10">
-                <div class="container">
-                    @section("content")
-                    @show
+            @component("components.sidebar")
+            @endcomponent
+                </div>
+                    <div class="col-10">
+                        <div class="container">
+                        @section("content")
+                        @show
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <!-- Bootstrap JS -->
-    <script src="/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
-    
-    <!-- jQuery -->
-    <script src="[https://code.jquery.com/jquery-3.7.1.min.js"></script>](https://code.jquery.com/jquery-3.7.1.min.js"></script>)
-    
-    <!-- DataTables JS -->
-    <script src="/DataTables/datatables.js"></script>
-</body>
+    </body>
 </html>
